a78a76d4621dcfc9ebde020c28fe05d5e3188fa8
ffashion <1323579954@qq.com>
2022-02-11 14:40:32 +0800

    add chan

--- gostudy/chan/chan.go
+++ gostudy/chan/chan.go
@@ -1,5 +1,18 @@
 package main
 
+//chan is type modifier
+//channel 在一个时刻只能有一个goroutine 访问通道并发送数据
 func main() {
-	// x chan;
+	string_chan := make(chan string)
+
+	//go 关键字 开启一个新的go routine
+	go func() {
+		//use channel output data
+		//this code will be blocked
+		string_chan <- "aaaa"
+	}()
+
+	data := <-string_chan
+	print(data, "\n")
+
 }
